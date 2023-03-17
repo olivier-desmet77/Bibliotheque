a04550de9cac05091444ec2b4351b51c483d2609
olivier-desmet77 <119798266+olivier-desmet77@users.noreply.github.com>
2023-03-17 12:01:31 +0100

    create, upgrade, list, supp

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -21,39 +21,76 @@ class AuthorController extends AbstractController
     public function create(Request $request, AuthorRepository $repository): Response
     {
         //tester si le formulaire est envoyé
-if ($request->isMethod('POST')) {
-    //récuperer les champs du formulaire
-    $name = $request->request->get('name');
-    $description = $request->request->get('description');
-    $imageUrl = $request->request->get('imageUrl');
-    //créer l'objet autheur avec les champs du form
-    $author = new author();
-    $author->setName($name);
-    $author->setDescription($description);
-    $author->setImageUrl($imageUrl);
-    //enregistrer l'auteur dans la bd via le repository
-    $repository->save($author, true);
+        if ($request->isMethod('POST')) {
+            //récuperer les champs du formulaire
+            $name = $request->request->get('name');
+            $description = $request->request->get('description');
+            $imageUrl = $request->request->get('imageUrl');
+            //créer l'objet autheur avec les champs du form
+            $author = new author();
+            $author->setName($name);
+            $author->setDescription($description);
+            $author->setImageUrl($imageUrl);
+            //enregistrer l'auteur dans la bd via le repository
+            $repository->save($author, true);
 
-    //redirection vers la liste
-}
-            //affichage du form de modification
-            return $this->render('admin/author/create.html.twig', [
-
-            ]);
-        
+            //redirection vers la liste
+            return $this->redirectToRoute('app_author_list');
+        }
+        //affichage du form de modification
+        return $this->render('admin/author/create.html.twig', []);
     }
 
-         //Lister les auteurs
-         #[Route('/author/liste', name:'app_author_list')]
-         public function list(AuthorRepository $repository):Response
-         {
-             //recuperer la liste des autheurs de la bd via le repo
-             $authors = $repository->findAll();
-             //afficher la liste dans le twig
-                 return $this->render('admin/author/list.html.twig', ['authors' => $authors]);
-         }
+    //Lister les auteurs
+    #[Route('/admin/auteurs/liste', name: 'app_author_list')]
+    public function list(AuthorRepository $repository): Response
+    {
+        //recuperer la liste des autheurs de la bd via le repo
+        $authors = $repository->findAll();
+        //afficher la liste dans le twig
+        return $this->render('admin/author/list.html.twig', ['authors' => $authors]);
+    }
 
+    //Mettre à jour un auteur
+    #[Route('/autheurs/{id}/modifier', name: 'app_autheurs_update')]
+    public function update(int $id, AuthorRepository $repository, Request $request): Response
+    {
+        //recuperer l'auteur avec l'id specifié dans la route
+        $author = $repository->find($id);
+        //tester si le formulaire est envoyé
+        if ($request->isMethod('POST')) {
+            //recuperer les champs du formulaire
+            $name = $request->request->get('name');
+            $description = $request->request->get('description');
+            $imageUrl = $request->request->get('imageUrl');
+            //modifier $author avec les nouvelles données
+            $author->setName($name);
+            $author->setDescription($description);
+            $author->setImageUrl($imageUrl);
+            //enregistrer dans la bd via le repo
+            $repository->save($author, true);
+            //redirection vers la liste des autheurs
+            return $this->redirectToRoute('app_author_list');
+        }
+        //affichage du form de modification
+        return $this->render('admin/author/update.html.twig', [
+            'author' => $author
+        ]);
+    }
 
+    //Supprimer un auteur
+    #[Route('/autheurs/{id}/supprimer', name:'app_author_remove')]
+    public function remove(int $id, AuthorRepository $repository):Response
+    {
+        //recupére la author à supprimer selon le id
+        $author = $repository->find($id);
+
+        //supprime la author de bd via le repo
+        $repository->remove($author, true);
+
+        //redirection vers la liste des autheurs
+        return $this->redirectToRoute('app_author_list');
+    }
 
 
 }
